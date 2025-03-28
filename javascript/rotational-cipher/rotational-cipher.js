a9413d17a58451a7c4c7038dde909ca690131ac1
Dev-Zayrak <calle.florian@gmail.com>
2025-03-28 19:27:10 +0100

    update javascript exercise rotational cipher

--- javascript/rotational-cipher/rotational-cipher.js
+++ javascript/rotational-cipher/rotational-cipher.js
@@ -1,8 +1,11 @@
-//
-// This is only a SKELETON file for the 'Rotational Cipher' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+/**
+ * 
+ * Writing the function in two different ways, the execution time is similar between the two.
+ * They take about 0.4... to execute all the tests.
+ * 
+ */
 
-const alphabet = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'  
+const alphabet = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-export const rotate = (cipherText, rot) => [...cipherText].reduce( (acc, char) => acc + (alphabet.indexOf(char) >= 0 ? alphabet[alphabet.indexOf(char) + rot] : char), '' )
+export const rotate = (cipherText, rot) => cipherText.replace(/[a-zA-Z]/g, char => alphabet[alphabet.indexOf(char) + rot])
+// export const rotate = (cipherText, rot) => [...cipherText].reduce( (acc, char) => acc + (alphabet.indexOf(char) >= 0 ? alphabet[alphabet.indexOf(char) + rot] : char), '' )
