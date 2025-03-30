0b52118d0e624ea10aa8c2ff604759190adf4fc8
Dev-Zayrak <calle.florian@gmail.com>
2025-03-30 15:44:49 +0200

    update javascript exercise protein translation

--- javascript/protein-translation/protein-translation.js
+++ javascript/protein-translation/protein-translation.js
@@ -3,6 +3,64 @@
 // convenience to get you started writing code faster.
 //
 
-export const translate = () => {
-  throw new Error('Remove this statement and implement this function');
+const proteins = new Map(Object.entries({
+  'AUG': 'Methionine',
+  'UUU': 'Phenylalanine',
+  'UUC': 'Phenylalanine',
+  'UUA': 'Leucine',
+  'UUG': 'Leucine',
+  'UCU': 'Serine',
+  'UCC': 'Serine',
+  'UCA': 'Serine',
+  'UCG': 'Serine',
+  'UAU': 'Tyrosine',
+  'UAC': 'Tyrosine',
+  'UGU': 'Cysteine',
+  'UGC': 'Cysteine',
+  'UGG': 'Tryptophan',
+  'UAA': 'STOP',
+  'UAG': 'STOP',
+  'UGA': 'STOP'
+}));
+
+
+
+//const getProteins = arr => arr.reduce((acc, value) => acc + (proteins.get(value) !== 'STOP' ? proteins.get(value) : ''), '')
+
+export const translate = (sequence) => {
+  
+  if(sequence === undefined) return []
+  /*
+    decouper la sequence par bloc de 3 caracteres
+    comparer les blocs avec les cles de PROTEIN
+    if value = stop -> break
+    mettre la valeur dans un tableau.
+    retourner le tableau
+  */
+
+    
+    
+    let sequences = sequence.match(/.{1,3}/g)
+    let output = []
+
+    for(let i = 0; i<sequences.length; i++){
+
+      let character = sequences[i]
+      if(proteins.get(character) === undefined) throw new Error('Invalid codon')
+      else if(proteins.get(character) === 'STOP') break
+      else output.push(proteins.get(character))
+    }
+
+    //console.log(output)
+    return output
+
+    
+    
+
+    // export const rotate = (cipherText, rot) => [...cipherText].reduce( (acc, char) => acc + (alphabet.indexOf(char) >= 0 ? alphabet[alphabet.indexOf(char) + rot] : char), '' )
+
+
+
+    // export const rotate = (cipherText, rot) => cipherText.replace(/[a-zA-Z]/g, char => alphabet[alphabet.indexOf(char) + rot])
+
 };
