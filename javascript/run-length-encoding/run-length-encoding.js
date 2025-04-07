a6533aead9e8262577c14cf3b363459dba775677
Dev-Zayrak <calle.florian@gmail.com>
2025-04-07 05:40:48 +0200

    update javascript exercise run length encoding optimization

--- javascript/run-length-encoding/run-length-encoding.js
+++ javascript/run-length-encoding/run-length-encoding.js
@@ -3,40 +3,14 @@
 // convenience to get you started writing code faster.
 //
 
-
-
 export const encode = str => {
-  
   if(str === '') return ''
 
-  let output = ''
-  let count = 1
-
-  for(let i = 0; i<str.length; i++){
-
-    if(count === 1 && str.charAt(i) !== str.charAt(i+1)) {
-      output += str.charAt(i)
-      continue
-    }
-    if(str.charAt(i) === str.charAt(i+1)){
-      count++
-      continue
-    }
-    if(str.charAt(i) !== str.charAt(i+1)){
-      output += count + str.charAt(i)
-      count = 1
-    }
-  }
-  return output
+  return str.match(/(.)\1*/g).map(element => element.length === 1 ? element : element.length + element.charAt(0)).join('')
 };
-
-const convertToArray = str => str.match(/\d+|[^\d]/g).map(element => Number.isInteger(parseInt(element)) ? parseInt(element) : element)
-const convert = (arr, str) => {
-  arr.forEach((element, i, arr) => Number.isInteger(element) ? str += arr[i+1].repeat(element-1) : str += element);
-  return str
-}
 
 export const decode = str => {
   if(str === '') return ''
-  return convert(convertToArray(str), '')
+
+  return str.match(/\d+|[^\d]/g).map((element, i, arr) => Number.isInteger(parseInt(element)) ? arr[i+1].repeat(element-1) : element).join('')
 };
