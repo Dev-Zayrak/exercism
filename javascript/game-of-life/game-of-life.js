36de403baa94776609adfef8feb0fbcae8884cff
Dev-Zayrak <calle.florian@gmail.com>
2025-04-11 07:22:16 +0200

    update javascript exercise game of life

--- javascript/game-of-life/game-of-life.js
+++ javascript/game-of-life/game-of-life.js
@@ -4,15 +4,36 @@
 //
 
 export class GameOfLife {
-  constructor() {
-    throw new Error('Remove this statement and implement this function');
+  constructor(matrix) {
+    this.matrix = matrix
   }
 
   tick() {
-    throw new Error('Remove this statement and implement this function');
+    
+    let output = this.matrix.map(row => row.slice());
+    
+    for(let i = 0; i< this.matrix.length; i++){
+      for(let y = 0; y<this.matrix[i].length; y++){
+
+        let topLeft = this.matrix[i-1] === undefined || this.matrix[i-1][y-1] === undefined ? 0 : this.matrix[i-1][y-1]
+        let topMid = this.matrix[i-1] === undefined || this.matrix[i-1][y] === undefined ? 0 : this.matrix[i-1][y]
+        let topRight = this.matrix[i-1] === undefined || this.matrix[i-1][y+1] === undefined ? 0 : this.matrix[i-1][y+1]
+        let left = this.matrix[i] === undefined  || this.matrix[i][y-1] === undefined ? 0 : this.matrix[i][y-1]
+        let right = this.matrix[i] === undefined || this.matrix[i][y+1] === undefined ? 0 : this.matrix[i][y+1]
+        let bottomLeft = this.matrix[i+1] === undefined || this.matrix[i+1][y-1] === undefined ? 0 : this.matrix[i+1][y-1]
+        let bottomMid = this.matrix[i+1] === undefined || this.matrix[i+1][y] === undefined ? 0 : this.matrix[i+1][y]
+        let bottomRight = this.matrix[i+1] === undefined || this.matrix[i+1][y+1] === undefined ? 0 : this.matrix[i+1][y+1]        
+
+        let countAlives = [topLeft, topMid, topRight, left, right, bottomLeft, bottomMid, bottomRight].filter(x => x === 1).length
+
+        countAlives === 3 ? output[i][y] = 1 : this.matrix[i][y] === 1 && countAlives === 2 ? output[i][y] = 1 : output[i][y] = 0  
+      }
+    }
+
+    this.matrix = output
   }
 
   state() {
-    throw new Error('Remove this statement and implement this function');
+    return this.matrix
   }
 }
