43edaa4b067a39d3de77626a7f3562b8c612e7c9
Dev-Zayrak <calle.florian@gmail.com>
2025-04-21 17:13:09 +0200

    update javascript exercise space age

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -3,6 +3,17 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this statement and implement this function');
+export const age = (planet, ageInSeconds) => {
+  const time = {
+    'mercury': 0.2408467,
+    'venus': 0.61519726,
+    'earth': 1.0,
+    'mars': 1.8808158,
+    'jupiter': 11.862615,
+    'saturn': 29.447498,
+    'uranus': 84.016846,
+    'neptune': 164.79132,
+  };
+
+  return Math.round(((ageInSeconds / 31557600) / time[planet])*100)/100;
 };
