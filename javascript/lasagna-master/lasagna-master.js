136d6506b8deef35c0ccedc173fedebb16e3e964
Dev-Zayrak <calle.florian@gmail.com>
2025-04-22 16:06:37 +0200

    update javascript exercise lasagna master

--- javascript/lasagna-master/lasagna-master.js
+++ javascript/lasagna-master/lasagna-master.js
@@ -10,3 +10,30 @@
  *   ...
  * }
  */
+
+export function cookingStatus(timeLeft){
+    return timeLeft === undefined ? 'You forgot to set the timer.' : timeLeft === 0 ? 'Lasagna is done.' : 'Not done, please wait.';
+}
+
+export function preparationTime(layers, timeByLayer = 2){
+    return layers.length * timeByLayer;
+}
+
+export function quantities(layers){
+    return {
+        noodles: layers.filter(ingredient => ingredient === 'noodles').length * 50,
+        sauce: layers.filter(ingredient => ingredient === 'sauce').length * 0.2,
+    };
+}
+
+export function addSecretIngredient(friendRecipients, myRecipients){
+    myRecipients.push(friendRecipients[friendRecipients.length-1]);
+}
+
+export function scaleRecipe(recipe, nbPortion){
+    const newRecipe = structuredClone(recipe);
+    for(const property in newRecipe){
+        newRecipe[property] = (newRecipe[property]/2) * nbPortion;
+    }
+    return newRecipe;
+}
