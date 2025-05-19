e50bf1f30e5c4f7211bae334c28089fbe7cdb5a9
Dev-Zayrak <calle.florian@gmail.com>
2025-05-19 18:31:37 +0200

    update javascript exercise pangram

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -3,14 +3,4 @@
 // convenience to get you started writing code faster.
 //
 
-export const isPangram = input => {
-  let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-  Array.from(input.toLowerCase()).forEach(character => {
-    if (alphabet.includes(character)){  
-      alphabet.splice(alphabet.indexOf(character), 1);
-    }
-  });
-
-  return alphabet.length === 0;
-};
+export const isPangram = text => new Set(text.toLowerCase().match(/[a-z]/g)).size === 26;
