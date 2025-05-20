c19e72cb3234420771e88efabebe65d2e0bd9fc8
Dev-Zayrak <calle.florian@gmail.com>
2025-05-20 20:06:20 +0200

    add javascript exercise acronym

---
+++ javascript/acronym/acronym.js
@@ -0,0 +1,6 @@
+//
+// This is only a SKELETON file for the 'Acronym' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const parse = sentence =>  sentence.toUpperCase().split(/[\s-]/).reduce((acc, word) => word ? acc + word.match(/[A-Z]/) : acc, '');
