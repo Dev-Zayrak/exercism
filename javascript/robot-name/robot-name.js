39192d0362463a32f8746ead103729e26dac0b24
Dev-Zayrak <calle.florian@gmail.com>
2025-06-04 21:11:12 +0200

    add javascript exercise robot name

---
+++ javascript/robot-name/robot-name.js
@@ -0,0 +1,51 @@
+// This is only a SKELETON file for the 'Robot Name' exercise. It's been
+// provided as a convenience to get your started writing code faster.
+
+export class Robot {
+    
+    static NAMES_LIST = new Set();
+    static TOTAL_NUMBER_OF_NAMES = 26 * 26 * 10 * 10 * 10;
+
+    constructor(){
+        if (Robot.NAMES_LIST.size === Robot.TOTAL_NUMBER_OF_NAMES){
+            throw new Error('Cannot create robot: no names available');
+        }
+        this.robotName = '';
+    }
+
+    get name(){
+        if (this.robotName === ''){
+            return this.robotName = Robot.releaseNames();
+        }
+        return this.robotName;
+    }
+
+    reset(){
+        this.robotName = '';
+    }
+}
+
+
+
+Robot.releaseNames = () => {
+
+    if (Robot.NAMES_LIST.size === Robot.TOTAL_NUMBER_OF_NAMES){
+        throw new Error('No names avalaible');
+    }
+
+    let name = '';
+    for (let i = 0; i< 2; i++){
+        name += String.fromCharCode(Math.floor(Math.random() * (91 - 65) + 65));
+    }
+    for (let i = 0; i < 3; i++){
+        name += Math.floor(Math.random() * 10)
+    }
+
+    if (Robot.NAMES_LIST.has(name)){
+        return Robot.releaseNames();
+    }
+    else{
+        Robot.NAMES_LIST.add(name);
+        return name;
+    }
+};
