732c95ff238aba67c90071148a3e8c15c2e528bc
Dev-Zayrak <calle.florian@gmail.com>
2025-06-19 10:48:21 +0200

    update javascript exercise secret handshake

--- javascript/secret-handshake/secret-handshake.js
+++ javascript/secret-handshake/secret-handshake.js
@@ -3,6 +3,28 @@
 // convenience to get you started writing code faster.
 //
 
-export const commands = () => {
-  throw new Error('Remove this statement and implement this function');
+const actions = [
+  'wink',
+  'double blink',
+  'close your eyes',
+  'jump'
+];
+
+export const commands = number => {
+  const binary = intToBinary(number);
+  let response = [];
+
+  for (let i = 0; i < 4; i++){
+    if (binary[i] === 1){
+      response.push(actions[i]);
+    }
+  };
+
+  return binary[4] === 1 ? response.reverse() : response;
+};
+
+const intToBinary = (int, binary = []) => {
+  const quotient = Math.floor(int / 2);
+  binary.push(int%2);
+  return quotient < 1 ?  binary : intToBinary(quotient, binary);
 };
